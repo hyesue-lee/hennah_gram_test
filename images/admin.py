2ee39375feb01180a112c72d4dc91db1db28a288
hyesueleee <34415068+hyesueleee@users.noreply.github.com>
2018-01-28 00:28:09 +0700

    git_db

--- images/admin.py
+++ images/admin.py
@@ -8,11 +8,56 @@ class ImageAdmin (admin.ModelAdmin):
         'created_by',
         'updated_at',
     )
+  
+    search_fields = (
+        'location',
+       
+    )
+    list_display_links = (
+        'caption',
+        
+    )
 
+    list_filter = (
+        'created_at',
+        'location',
+        'created_by',
+        
+    )
+
+    ordering = (
+        'location',
+    )
+
+class CommentAdmin (admin.ModelAdmin):
+    list_display = (
+        'message',
+        'created_by',
+        'created_for',
+        'updated_at',
+        'created_at',
+    )
+    list_display_links = (
+        'created_for',
+        'message',
+    )    
+    list_filter = (
+        'message',
+        'updated_at',
+        'created_at',
+    )    
+
+    search_fields = (
+        'message',
+        'updated_at',
+        'created_at',
+    )
+  
 # Register your models here.
 admin.site.register(Image, ImageAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Like)
+
 
 
 
