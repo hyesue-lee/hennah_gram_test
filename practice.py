776d97ca9f2c139e0dceab751af45b14bd1976cf
hyesueleee <34415068+hyesueleee@users.noreply.github.com>
2018-01-27 18:26:32 +0700

    dd

---
+++ practice.py
@@ -0,0 +1,19 @@
+def plus(x,y):
+    print (x+y)
+plus (3,2)
+
+def minus(x,y):
+    print(x-y)
+minus (3,2)
+
+def times(x,y):
+    print(x*y)
+times (3,2)
+
+def divide(x,y):
+    print (x/y)
+divide (3,2)
+
+def power(x,y):
+    print (x**y)
+power (3,2)
