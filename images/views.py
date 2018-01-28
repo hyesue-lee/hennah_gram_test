ca7b684561cbb2a277d519353280de4501ef74b7
hyesueleee <34415068+hyesueleee@users.noreply.github.com>
2018-01-29 02:25:08 +0700

    backend

--- images/views.py
+++ images/views.py
@@ -7,7 +7,7 @@ def index(request):
     user = request.user
     if user.is_authenticated:
         all_images = Image.objects.all()
-        return render(request, 'feed.html', context = {
+        return render(request, 'base.html', context = {
             'images': all_images
         })
     else:
@@ -17,21 +17,27 @@ def explore(request):
     user = request.user
     if user.is_authenticated:
         all_images = Image.objects.all()
-        return render(request, 'explore.html',)
+        return render(request, 'explore.html', context = {
+            'images': all_images} )
     else:
         return render(request, 'login.html',)
 
 def feed(request):
     user = request.user
     if user.is_authenticated:
-        return render(request, 'feed.html',)
+        all_images = Image.objects.all()
+        
+        return render(request, 'feed.html', context = {
+            'images': all_images} )
     else:
         return render(request, 'login.html',)        
 
 def profile(request):
     user = request.user
     if user.is_authenticated:
-        return render(request, 'profile.html',)
+        all_images = Image.objects.all()
+        return render(request, 'profile.html', context = {
+            'images': all_images} )
     else:
         return render(request, 'login.html',)
 
